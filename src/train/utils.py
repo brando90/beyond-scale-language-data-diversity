3491bd36c63032c5805bea8c0664f47305cf624d
Brando Miranda <brandojazz@gmail.com>
2024-01-17 15:18:02 -0800

    starter code to train llama, abstracting for resuability the block size continuation code

--- src/train/utils.py
+++ src/train/utils.py
@@ -11,22 +11,35 @@ import random
 
 import torch
 
+import datasets
+from datasets import load_dataset, interleave_datasets
+
 from transformers import PreTrainedTokenizer, AutoTokenizer, Trainer, TrainingArguments, AutoConfig
 from transformers.testing_utils import CaptureLogger
 
-def get_column_names(dataset, spit: str = 'train', method: str = 'features'):
+def get_column_names(dataset, spit: str = 'train', method: str = 'features', streaming: bool = True):
     if method == 'features':
-        column_names = next(iter(dataset)).keys()
-    elif method == 'keys':
         column_names = list(dataset[spit].features)
+    elif method == 'keys':
+        # batch = get_data_from_hf_dataset(dataset, streaming=streaming, batch_size=1)
+        # column_names = next(iter(batch)).keys()
+        column_names = next(iter(dataset)).keys()
     else:
         raise ValueError(f"method {method} not supported")
     return column_names
 
-def get_data_from_hf_dataset(dataset, streaming: bool = True, batch_size: int = 4):
+def get_data_from_hf_dataset(dataset, 
+                             streaming: bool = True, 
+                             batch_size: int = 4, 
+                             shuffle: bool= False, # shuffle is better but slower afaik
+                             seed: int = 0, 
+                             buffer_size: int = 500_000,
+                             ):
     """ Gets data from a HF dataset, it's usually an iterator object e.g., some ds.map(fn, batched=True, remove_columns=remove_columns) has been applied. 
     Handles both streaming and non-streaming datasets, take for streaming and select for non-streaming.
     """
+    dataset = dataset.shuffle(buffer_size=buffer_size, seed=seed) if shuffle else dataset
+    # sample_data = dataset.select(range(batch_size)) if not isinstance(dataset, datasets.iterable_dataset.IterableDataset) else dataset.take(batch_size)
     batch = dataset.take(batch_size) if streaming else dataset.select(random.sample(list(range(len(dataset))), batch_size))
     return batch
 
@@ -59,6 +72,10 @@ def tokenize_function(examples, tokenizer, text_column_name: str):
     Assumes batched=True so examples is many row/data points.
     """
     return tokenizer(examples["text_column_name"])
+
+def preprocess(examples, tokenizer, max_length: int = 1024):
+    return tokenizer(examples["text"], padding="max_length", max_length=max_length, truncation=True, return_tensors="pt")
+    # return tokenizer(examples["text"], padding="max_length", max_length=model.config.context_length, truncation=True, return_tensors="pt")
 
 def group_texts(examples, block_size: int = 4096):
     """
@@ -150,7 +167,8 @@ def _test_all_batches_are_size_block_size():
     # load c4 data set hf in streaming mode 
     from datasets import load_dataset
     streaming = True
-    raw_datasets = load_dataset("c4", "en", streaming=streaming)
+    raw_datasets = load_dataset("c4", "en", streaming=streaming).with_format("torch")
+    get_data_from_hf_dataset(raw_datasets, streaming=streaming, batch_size=batch_size) 
     remove_columns = get_column_names(raw_datasets)  # remove all keys that are not tensors to avoid bugs in collate function in task2vec's pytorch data loader
 
     # how does it know which column to tokenize? gpt4 says default is text or your tokenized function can specify it, see my lambda fun above
@@ -159,11 +177,19 @@ def _test_all_batches_are_size_block_size():
         batched=True,  # Setting `batched=True` in the `dataset.map` function of Hugging Face's datasets library processes the data in batches rather than one item at a time, significantly speeding up the tokenization and preprocessing steps.
         remove_columns=remove_columns,
     )
-
+    get_data_from_hf_dataset(tokenized_datasets, streaming=streaming, batch_size=batch_size)
     lm_datasets = tokenized_datasets.map(
         group_texts,
-        batched=True,
+        batched=True,  # Setting `batched=True` in the `dataset.map` function of Hugging Face's datasets library processes the data in batches rather than one item at a time, significantly speeding up the tokenization and preprocessing steps.
     )
+    get_data_from_hf_dataset(lm_datasets, streaming=streaming, batch_size=batch_size)
+
+    # get batch
+    batch = get_data_from_hf_dataset(lm_datasets, streaming=streaming, batch_size=batch_size)
+    print(batch)
 
 if __name__ == "__main__":
-    pass
+    from time import time
+    start_time = time()
+    _test_all_batches_are_size_block_size()
+    print(f"Done!\a Total time: {time() - start_time} seconds, or {(time() - start_time)/60} minutes. or {(time() - start_time)/60/60} hours.\a")
