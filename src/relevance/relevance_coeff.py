d0a09a2b26a0aa3535159a7fd7606ead95f8d43d
Brando Miranda <miranebr@amazon.com>
2024-08-30 15:38:56 -0700

    relevance coeff works

--- src/relevance/relevance_coeff.py
+++ src/relevance/relevance_coeff.py
@@ -7,6 +7,7 @@ import glob
 from diversity.task2vec import Task2Vec 
 from diversity import task_similarity
 from diversity.div_coeff import cross_diversity_coefficient
+from diversity.utils import seed_everything
 
 from pathlib import Path
 
@@ -25,9 +26,9 @@ def relevance_coeff_task2vec_via_full_embed_dataset(
         dataset_source,
         dataset_target,
         probe_network: nn.Module,
-        batch_size: int = 8,
+        task_size: int = 50,  # Size of dataset to compute t2v, note: there is a batch_size param for the t2v itself
+        batch_size: int = 8, # Size of batch size for FT fir t2v, so the task_size/batch_size is the number of iters for final layer for t2v
         seed: int = 42, 
-        buffer_size: int = 500_000, 
         distance = 'cosine',
         verbose: bool = False,
         debug: bool = False,
@@ -46,26 +47,28 @@ def relevance_coeff_task2vec_via_full_embed_dataset(
 
     Note: there is no sense of number of batches here, so num_batches = 1 effectively + if CIs needed need to be with wrt batch examples. 
     """
+    seed_everything(seed) if seed is not None else None
+
     # - Get target shuffled data
-    shuffled_dataset = dataset_source.shuffle(buffer_size=buffer_size, seed=seed) if shuffle else dataset_source
-    batch = shuffled_dataset.take(batch_size)
+    shuffled_dataset = dataset_source.shuffle(seed=seed) if shuffle else dataset_source
+    data = shuffled_dataset.take(task_size)
     
     # - Get Task2Vec embedding for batch
     if not debug:
-        embedding_target, loss_target = Task2Vec(probe_network, classifier_opts={'seed': seed}).embed(batch)
+        embedding_target, loss_target = Task2Vec(probe_network, classifier_opts={'batch_size': batch_size}).embed(data)
     else:
-        embedding_target, loss_target = Task2Vec(probe_network, classifier_opts={'break_early': True, 'seed': seed}).embed(batch, epochs=1)  # only for debugging
+        embedding_target, loss_target = Task2Vec(probe_network, classifier_opts={'break_early': True}).embed(data, epochs=1)  # only for debugging
     print(f'{loss_target=}\n{embedding_target=}\n') if verbose else None
 
     # - Get source shuffled data
-    shuffled_dataset = dataset_target.shuffle(buffer_size=buffer_size, seed=seed)
-    batch = shuffled_dataset.take(batch_size)
+    shuffled_dataset = dataset_target.shuffle(seed=seed)
+    data = shuffled_dataset.take(task_size)
     
     # - Get Task2Vec embedding for batch
     if not debug:
-        embedding_source, loss_source = Task2Vec(probe_network, classifier_opts={'seed': seed}).embed(batch)
+        embedding_source, loss_source = Task2Vec(probe_network, classifier_opts={'seed': seed}).embed(data)
     else:
-        embedding_source, loss_source = Task2Vec(probe_network, classifier_opts={'break_early': True, 'seed': seed}).embed(batch, epochs=1)  # only for debugging
+        embedding_source, loss_source = Task2Vec(probe_network, classifier_opts={'break_early': True, 'seed': seed}).embed(data, epochs=1)  # only for debugging
     print(f'{loss_target=}\n{embedding_target=}\n') if verbose else None
 
     # - Store results 
@@ -83,7 +86,7 @@ def relevance_coeff_task2vec_via_full_embed_dataset(
                     'embeddings': embeddings,
                     'distance_matrix': distance_matrix,
                     'losses': losses,
-                    'batch_size': batch_size}
+                    'task_size': task_size, 'batch_size': batch_size}
     return results
 
 # -- Tests
@@ -93,26 +96,26 @@ def load_mdl_and_tok(pretrained_model_name_or_path):
     mdl = AutoModelForCausalLM.from_pretrained(pretrained_model_name_or_path, torch_dtype=torch_dtype, trust_remote_code=True)
     device = torch.device(f"cuda:{0}" if torch.cuda.is_available() else "cpu")
     mdl = mdl.to(device)
-    tok = AutoTokenizer.from_pretrained(pretrained_model_name_or_path, padding_side="right", trust_remote_code=True)
+    tok = AutoTokenizer.from_pretrained(pretrained_model_name_or_path, padding_side="right", trust_remote_cseedode=True)
     tok.pad_token = tok.eos_token if tok.pad_token_id is None else tok.pad_token
     return mdl, tok
 
-def load_math_test(dataset_source, tokenizer, num_proc=None, batched: bool = True, max_length=128, truncation=True):
+def load_math_test(dataset_source, tokenizer, num_proc=None, batched: bool = True, max_length=128, truncation=True, end: int = 100):
     jsonl_files: list[str] = glob.glob(os.path.expanduser(dataset_source), recursive=True)
     assert len(jsonl_files) > 1, f'You have some error only 1 file, likely you are missing /**/*.json from your path to the data set {dataset_source=}.'
-    dataset_source = load_dataset('json', data_files=jsonl_files, split='test')
-    ds_src = load_dataset(dataset_source)
+    jsonl_files = jsonl_files[:end]
+    ds_src = load_dataset('json', data_files=jsonl_files, split='train')
     # to train str
-    raw_str_2_train_str = lambda examples : {'text': [f'problem: {prob}\n\nsolution: {sol}' for prob, sol in zip(examples['problems'], examples['solutions'])]}
+    raw_str_2_train_str = lambda examples : {'text': [f'problem: {prob}\n\nsolution: {sol}' for prob, sol in zip(examples['problem'], examples['solution'])]}
     train_str_ds = ds_src.map(raw_str_2_train_str, batched=batched, num_proc=num_proc)
     # to train tokenized ds
-    tok_ds = tok_ds.with_format('torch')  
+    tok_ds = train_str_ds.with_format('torch')  
     tokenize_function = lambda examples: tokenizer(examples['text'], padding='max_length', max_length=max_length, truncation=truncation, return_tensors='pt')
-    tok_ds = train_str_ds.map(tokenize_function, batched=batched, remove_columns=train_str_ds.column_names, num_proc=num_proc)
+    tok_ds = tok_ds.map(tokenize_function, batched=batched, remove_columns=tok_ds.column_names, num_proc=num_proc)
     # get lm dataset
     # usually you do group_texts, packing, or mask extra eos's
     lm_ds = tok_ds
-    return lm_ds, lm_ds
+    return lm_ds
 
 def _test_sanity_check_align_to_yourself_is_very_high_or_1(
         dataset_source: str = '~/beyond-scale-language-data-diversity/data/MATH/test/**/*.json',
